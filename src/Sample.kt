916a238c4e91fc89afc58cb99ad43186ab1b6ae2
neriudon <tamura.t1986@gmail.com>
2018-10-23 22:20:51 +0900

    study about class

--- src/Sample.kt
+++ src/Sample.kt
@@ -2,10 +2,66 @@
  * main function.
  */
 fun main(args: Array<String>) {
+    val name = if (args.isNotEmpty()) args[0] else "Foo"
+    println("Hello $name !")
+    if (args.isNotEmpty()) println("Hello ${args[0]}!")
+    println("Hello ${if (args.isNotEmpty()) args[0] else "Hoge"}!")
+    var person = Person("Tom", true)
+    person.isMarried = false
+    println(person.name + person.isMarried)
+}
+
+/**
+ * Kotlin class.
+ * This class means...
+ *
+ * <pre>
+ *     public class Person{
+ *       private final String name;
+ *       private boolean isMarried;
+ *       Person(String name, boolean isMarried){
+ *         this.name = name;
+ *         this.isMarried = isMarried;
+ *       }
+ *       public String getName(){
+ *         return name;
+ *       }
+ *       public boolean isMarried(){
+ *         return isMarried;
+ *       }
+ *       public void setMarried(boolean isMarried){
+ *         this.isMarried = isMarried;
+ *       }
+ *   }
+ *   </pre>
+ */
+class Person(val name :String, var isMarried: Boolean)
+
+
+
+
+fun demo() {
     println("Hello World!")
     println(max(5, 10))
     println(maxShort(5, 10))
     println(maxMoreShort(5, 10))
+    // no need to define type
+    val x = "hogehoge"
+    val y = 1
+    val z: Int = 0
+    val w = 7.5e6 // this is double
+    //val u:Float = 7.5e6 // this is NG
+    val flag: Boolean
+    if (Math.random() * 2 > 1) {
+        flag = true
+    } else {
+        flag = false
+    }
+    // flag = true // val can not be changed
+    var flag2 = true
+    flag2 = false // var chan be changed
+    val list = arrayListOf("aaa")
+    list.add("bbb")
 }
 
 /**
